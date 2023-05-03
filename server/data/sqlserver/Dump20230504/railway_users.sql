d30a6d4953c86c76eb44c6ce4141884d4320702e
I99900K <truongvans1010@gmail.com>
2023-05-04 01:52:55 +0700

    up script

---
+++ server/data/sqlserver/Dump20230504/railway_users.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:264639f3aaef3d6f157c201880b54c8e798dba5b6ea2b445ef68f3a365b828b8
+size 2586
---
+++ server/data/sqlserver/Dump20230504/railway_wishlist.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf6a06a91e8340a33e5eb9391c6d056f655c762535fbc0f86364d471db25a888
+size 2356
