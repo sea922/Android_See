59c0d8c32a60a88495b483cc6f7d50ba8935444d
I99900K <truongvans1010@gmail.com>
2023-05-05 12:19:16 +0700

    add map screen and change layout

---
+++ androidApp/app/src/main/java/com/example/seeStore/activity/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.seeStore.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.seeStore.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
